b901d2db89d1e46640c0758af5e669bd247ff39b
Saoki11111 <t.aoki11111@gmail.com>
2020-09-26 20:08:26 +0900

    init

---
+++ disemvowel_trolls.rb
@@ -0,0 +1,13 @@
+# トロールがコメントセクションを攻撃しています！
+# 
+# この状況に対処する一般的な方法は、トロールのコメントからすべての母音を削除して、脅威を中和することです。
+# 
+# あなたの仕事は、文字列を取り、すべての母音が削除された新しい文字列を返す関数を書くことです。
+# 
+# たとえば、「This website is for losers LOL!」という文字列です。 「Ths wbst s fr lsrs LL！」になります。
+
+str = ('This website is for losers LOL!')
+
+s = 'aiueo'
+
+puts s.include?(str)
